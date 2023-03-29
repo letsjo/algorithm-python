168e6d80ae6d135311246373d306a882bc5cd981
letsjo <hyunoh.jo@gmail.com>
2023-03-30 02:23:13 +0900

    [#3] Fix: Update 윤년
    윤년 조건을 잘못 이해하고 입력했었는데, 답이 맞다고 나왔음
    조건 수정 후 재업로드

--- 2753.py
+++ 2753.py
@@ -1,9 +1,13 @@
+# 윤년
+
 year = int(input())
 
-isMultipleFour = year % 4 == 0
-isMultipleOneHundredOrFourHundred = (year % 100 != 0) | (year % 400 == 0)
+# 윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때
+# 또는 400의 배수일 때이다.
+isLeapYearCondition1 = year % 4 == 0 and year % 100 != 0
+isLeapYearCondition2 = year % 400 == 0
 
-if (isMultipleFour & isMultipleOneHundredOrFourHundred):
+if (isLeapYearCondition1 or isLeapYearCondition2):
     print(1)
 else:
     print(0)
