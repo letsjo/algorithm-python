95fbe27ca4c12bb2d616bb6d3a5b559f936f3f82
letsjo <hyunoh.jo@gmail.com>
2023-03-30 04:37:00 +0900

    [#5] Feat: Add 개수 세기

---
+++ 10807.py
@@ -0,0 +1,12 @@
+# 개수 세기
+
+N = int(input())
+numbers = list(map(int, input().split(' ')))
+v = int(input())
+count = 0
+
+for number in numbers:
+    if (number == v):
+        count += 1
+
+print(count)
