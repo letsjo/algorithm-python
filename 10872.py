c3ccb73fd4b98f4c378f27b80841968d93b6239f
letsjo <hyunoh.jo@gmail.com>
2023-04-08 14:23:15 +0900

    [#12] Feat: Update 팩토리얼
    재귀함수 로 로직 변경

--- 10872.py
+++ 10872.py
@@ -1,13 +1,10 @@
 # 팩토리얼
 
-N = int(input())
+def solution(n):
+    if (n == 0 | n == 1):
+        return 1
 
-if (N == 0 | N == 1):
-    print(1)
+    return n*solution(n-1)
 
-else:
-    answer = 1
-    for i in range(2, N+1):
-        answer *= i
 
-    print(answer)
+print(solution(int(input())))
