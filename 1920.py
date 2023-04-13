11cd3a770d019b140c809a28b27a701ada511d4b
letsjo <hyunoh.jo@gmail.com>
2023-04-13 23:17:38 +0900

    [#13] Feat: Update 수 찾기
    이진 탐색 재귀로 풀이법 변경

--- 1920.py
+++ 1920.py
@@ -1,12 +1,23 @@
 # 수 찾기
 
 N = int(input())
-checkList = sorted(list(map(int, input().split())))
+sortedArr = sorted(list(map(int, input().split())))
 M = int(input())
 findList = list(map(int, input().split()))
 
+
+def binarySearch(start, end, target):
+    if (start > end):
+        return -1
+    mid = (start+end)//2
+
+    if (sortedArr[mid] == target):
+        return mid
+    if (sortedArr[mid] > target):
+        return binarySearch(start, mid-1, target)
+    return binarySearch(mid+1, end, target)
+
+
 for find in findList:
-    if find in checkList:
-        print('1')
-    else:
-        print('0')
+    index = binarySearch(0, len(sortedArr)-1, find)
+    print('1' if index >= 0 else '0')
