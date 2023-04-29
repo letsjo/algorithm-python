282b6fc0d6d40eff921e0499ab3e14c60ff74a61
letsjo <hyunoh.jo@gmail.com>
2023-04-29 16:43:06 +0900

    [#15] Feat: Add 가장 긴 증가하는 부분 수열
    DP 로 문제 해결

--- 11053.py
+++ 11053.py
@@ -2,43 +2,14 @@
 
 # Dynamic Programming
 
-# N = int(input())
-# A = list(map(int, input().split()))
-
-# dp = [1 for _ in range(N)]
-
-# for i in range(N):
-#     for j in range(i):
-#         if A[i] > A[j]:
-#             dp[i] = max(dp[i], dp[j]+1)
-
-# print(dp)
-
-# Binary Search, Stack
-
 N = int(input())
 A = list(map(int, input().split()))
 
-stack = []
+dp = [1 for _ in range(N)]
 
+for i in range(N):
+    for j in range(i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j]+1)
 
-def binarySearch(arr, start, end, target):
-    if (start >= end):
-        return start
-    mid = (start + end) // 2
-
-    if (arr[mid] == target):
-        return mid
-    if (arr[mid] < target):
-        return binarySearch(arr, mid+1, end, target)
-    return binarySearch(arr, start, mid, target)
-
-
-for a in A:
-    if not stack or (stack[-1] < a):
-        stack.append(a)
-    else:
-        i = binarySearch(stack, 0, len(stack), a)
-        stack[i] = a
-
-print(len(stack))
+print(max(dp))
