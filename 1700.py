eb08a8e77003ec56b16b79eed8a58dc28add9fb0
letsjo <hyunoh.jo@gmail.com>
2023-05-01 20:07:02 +0900

    [#15] Feat: Add 멀티탭 스케줄링

---
+++ 1700.py
@@ -0,0 +1,30 @@
+# 멀티탭 스케줄링
+
+N, K = map(int, input().split())
+
+orders = list(map(int, input().split()))
+
+supply = [0]*(N)
+count = 0
+
+for i in range(K):
+    if orders[i] in supply:
+        continue
+    elif 0 in supply:
+        supply[supply.index(0)] = orders[i]
+        continue
+    count += 1
+    pop_index = i
+    supply_index = N-1
+    for j in range(N):
+        if supply[j] in orders[i:]:
+            tem_index = orders.index(supply[j], i)
+            if tem_index > pop_index:
+                pop_index = tem_index
+                supply_index = j
+        else:
+            supply_index = j
+            break
+    supply[supply_index] = orders[i]
+
+print(count)
