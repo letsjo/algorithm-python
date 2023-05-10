4576b431168e76b0239fee4bfab57e08829b4d7e
letsjo <hyunoh.jo@gmail.com>
2023-05-10 10:40:37 +0900

    Feat: Update 1로 만들기
    dp로 풀기

--- 1463.py
+++ 1463.py
@@ -1,32 +1,56 @@
 # 1로 만들기
-import sys
-from collections import deque
 
+# DP로 풀기
+import sys
 input = sys.stdin.readline
 
 N = int(input())
-queue = deque()
-visited = [0]*(N+1)
-queue.append(N)
+dp = [0]*(N+1)
 
-while queue:
-    number = queue.popleft()
-    if number == 1:
-        break
+dp[1] = 0
 
-    if number % 3 == 0 and visited[int(number//3)] == 0:
-        queue.append(int(number//3))
-        visited[int(number//3)] += visited[number] + 1
+if (N > 1):
+    dp[2] = 1
+    for i in range(3, N+1):
+        if i % 3 == 0 and i % 2 == 0:
+            dp[i] = min(dp[i//3], dp[i//2], dp[i-1])+1
+        elif i % 3 == 0:
+            dp[i] = min(dp[i//3], dp[i-1])+1
+        elif i % 2 == 0:
+            dp[i] = min(dp[i//2], dp[i-1])+1
+        else:
+            dp[i] = dp[i-1]+1
+print(dp[N])
 
-    if number % 2 == 0 and visited[int(number//2)] == 0:
-        queue.append(int(number//2))
-        visited[int(number//2)] += visited[number] + 1
+# BFS 로 풀기
+# import sys
+# from collections import deque
 
-    if visited[number-1] == 0:
-        queue.append(number-1)
-        visited[number-1] += visited[number]+1
+# input = sys.stdin.readline
 
-print(visited[1])
+# N = int(input())
+# queue = deque()
+# visited = [0]*(N+1)
+# queue.append(N)
+
+# while queue:
+#     number = queue.popleft()
+#     if number == 1:
+#         break
+
+#     if number % 3 == 0 and visited[int(number//3)] == 0:
+#         queue.append(int(number//3))
+#         visited[int(number//3)] += visited[number] + 1
+
+#     if number % 2 == 0 and visited[int(number//2)] == 0:
+#         queue.append(int(number//2))
+#         visited[int(number//2)] += visited[number] + 1
+
+#     if visited[number-1] == 0:
+#         queue.append(number-1)
+#         visited[number-1] += visited[number]+1
+
+# print(visited[1])
 
 # dfs 는 최소를 구하는데 적합하지 않음
 # solution(10) -> 4
