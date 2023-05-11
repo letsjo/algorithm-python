fb6efc73ae5e99b41e08b70cc66f806f90653463
letsjo <hyunoh.jo@gmail.com>
2023-05-11 09:18:55 +0900

    Feat: Add 색종이 2차

---
+++ 2563-1.py
@@ -0,0 +1,15 @@
+# 색종이 2차
+
+countPaper = int(input())
+canvas = [[0]*101 for _ in range(101)]
+answer = 0
+
+for _ in range(countPaper):
+    x, y = map(int, input().split())
+    for i in range(x, x+10):
+        for j in range(y, y+10):
+            if canvas[i][j] == 0:
+                canvas[i][j] = 1
+                answer += 1
+
+print(answer)
