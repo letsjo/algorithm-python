3615329009294a43ed2ffe46e596a9c762802ce0
letsjo <hyunoh.jo@gmail.com>
2023-05-18 09:27:57 +0900

    Feat: Add 프린터 큐

---
+++ 1966.py
@@ -0,0 +1,21 @@
+# 프린터 큐
+
+T = int(input())
+
+for _ in range(T):
+    N, M = map(int, input().split())
+    queue = list(map(int, input().split()))
+    queue = [(queue[idx], idx) for idx in range(N)]
+    count = 0
+
+    while True:
+        if queue[0][0] == max(queue, key=lambda x: x[0])[0]:
+            count += 1
+            if queue[0][1] == M:
+                break
+            else:
+                queue.pop(0)
+        else:
+            queue.append(queue.pop(0))
+
+    print(count)
