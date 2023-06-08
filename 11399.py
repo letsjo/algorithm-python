2554ed4b815d72f7ebdb2d884aebdbdf41ac8425
letsjo <hyunoh.jo@gmail.com>
2023-06-08 10:35:44 +0900

    Feat: Add ATM with insert sort

--- 11399.py
+++ 11399.py
@@ -3,7 +3,15 @@
 n = int(input())
 p = list(map(int, input().split()))
 
-p.sort()
+# p.sort()
+# insert sort
+for i in range(1, n):
+    key = p[i]
+    j = i - 1
+    while j >= 0 and p[j] > key:
+        p[j + 1] = p[j]
+        j -= 1
+    p[j + 1] = key
 
 sum = 0
 for i in range(n):
